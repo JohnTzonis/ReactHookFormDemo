ad6ce2bf3eedfad3bb1715531a6b6054aebe631d
GiannisTzonis <tzonis@transifex.com>
2023-07-22 13:57:21 +0300

    added handleSubmit functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,43 +1,48 @@
 import { useForm } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 
+let renderCount = 0;
+
 export const Form = () => {
-    // returns an object
-    const form = useForm()
-    const { register, control } = form
+  const form = useForm();
+  const { register, control, handleSubmit } = form;
 
-    return (
-        <div>
-            <form>
-                <label htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    id="username"
-                    {...register("username")}
-                    >
-                </input>
+  const onSubmit = (data) => {
+    console.log('Form Submitted', data);
+  };
 
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    {...register("emain")}
-                    >
-                </input>
+  renderCount++;
 
-                <label htmlFor="channel">Channel</label>
-                <input
-                    type="text"
-                    id="channel"
-                    {...register("channel")}
-                    >
-                </input>
+  return (
+    <div>
+      <h1>Form ({renderCount / 2})</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          {...register('username')}
+        ></input>
 
-                <button>Submit</button>
-            </form>
-            <DevTool control={control} />
-            {/* touched indicates if the field has been interacted
-            dirty indicates if the field has been changed */}
-        </div>
-    )
-}
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          {...register('email')}
+        ></input>
+
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          {...register('channel')}
+        ></input>
+
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+      {/* touched indicates if the field has been interacted
+          dirty indicates if the field has been changed */}
+    </div>
+  );
+};
