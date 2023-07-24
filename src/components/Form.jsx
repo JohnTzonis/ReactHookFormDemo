256cc4a05c1f0391e709d2e026fad3f117b41cc0
GiannisTzonis <tzonis@transifex.com>
2023-07-24 12:39:27 +0300

    added validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,8 @@ let renderCount = 0;
 
 export const Form = () => {
   const form = useForm();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit , formState } = form;
+  const { errors } = formState;
 
   const onSubmit = (data) => {
     console.log('Form Submitted', data);
@@ -16,27 +17,52 @@ export const Form = () => {
   return (
     <div>
       <h1>Form ({renderCount / 2})</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          {...register('username')}
-        ></input>
-
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          {...register('email')}
-        ></input>
-
-        <label htmlFor="channel">Channel</label>
-        <input
-          type="text"
-          id="channel"
-          {...register('channel')}
-        ></input>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input
+            type="text"
+            id="username"
+            {...register('username', {
+                required: {
+                    value: true,
+                    message: "Username is required",
+                }
+            })}
+            ></input>
+            <p className="error">{errors.username?.message}</p>
+        </div>
+        
+        <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input
+            type="email"
+            id="email"
+            {...register('email', {
+                pattern: {
+                    value: /^[a-zA-Z0-9. !#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: 'Invalid email format'
+                }
+            })}
+            ></input>
+            <p className="error">{errors.email?.message}</p>
+        </div>
+        
+        <div className="form-control">
+            <label htmlFor="channel">Channel</label>
+            <input
+            type="text"
+            id="channel"
+            {...register('channel', {
+                required: {
+                    value: true,
+                    message: "Channel is required",
+                }
+            })}
+            ></input>
+            <p className="error">{errors.channel?.message}</p>
+        </div>
+        
 
         <button>Submit</button>
       </form>
